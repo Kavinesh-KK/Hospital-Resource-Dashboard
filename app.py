61d9e5a8f35e6beddc5ed34f7282f56c1f5ba1b3
Kavinesh-KK <kavinkk300304@gmail.com>
2026-01-31 16:54:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import pandas as pd
+
+app = FastAPI()
+
+df = pd.read_csv("dataset/hospital_data.csv")
+
+@app.get("/")
+def home():
+    return {"message": "Hospital Analytics API Running"}
+
+@app.get("/data")
+def get_data():
+    return df.to_dict(orient="records")
